2a00850af73ce253b266dc342104e4366da60444
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-06-22 23:00:39 +0100

    The swap opcode

---
+++ swap.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+
+/**
+* f_swap - swap first two elements of stack
+* @head: first node
+* @counter: line counter
+*
+* Return: none
+*/
+
+void f_swap(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int length = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	temp = h->n;
+	h->n = h->next->n;
+	h->next->n = temp;
+}
