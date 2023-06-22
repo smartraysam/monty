7f39e95fc4af8f560550f841b53ec66d1a0d0633
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-06-23 00:13:33 +0100

    The rotl opcode

--- rotl.c
+++ rotl.c
@@ -27,31 +27,3 @@ void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
 	(*head)->prev = tmp;
 	(*head) = aux;
 }
-
-/**
- * f_rotr - rotate stack element to the bottom
- * @head: first node of stack
- * @counter: line counter
- *
- * Return: none
- */
-
-void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
-{
-	stack_t *copy;
-
-	copy = *head;
-	if (*head == NULL || (*head)->next == NULL)
-	{
-		return;
-	}
-	while (copy->next)
-	{
-		copy = copy->next;
-	}
-	copy->next = *head;
-	copy->prev->next = NULL;
-	copy->prev = NULL;
-	(*head)->prev = copy;
-	(*head) = copy;
-}
