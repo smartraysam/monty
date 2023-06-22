7dc7296622bc63c9e09a6bbcc2f0bc24e244da89
smartraysam <raysamtob@gmail.com>
2023-06-22 23:39:01 +0100

    Add task 11

--- func_exec.c
+++ func_exec.c
@@ -17,6 +17,13 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 				{"add", f_add},
 				{"nop", f_nop},
 				{"sub", f_sub},
+				{"div", f_div},
+				{"mul", f_mul},
+				{"mod", f_mod},
+				{"pchar", f_pchar},
+				{"pstr", f_pstr},
+				{"rotl", f_rotl},
+				{"rotr", f_rotr},
 				{"queue", f_queue},
 				{"stack", f_stack},
 				{NULL, NULL}
