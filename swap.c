d2c1b65e06b8aecb3a3154dfc5d5a8ac557e26b7
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-06-22 23:02:34 +0100

    remove mod

--- swap.c
+++ swap.c
@@ -41,36 +41,3 @@ void f_swap(stack_t **head, unsigned int counter)
  * Return: none
  */
 
-void f_mod(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	int length = 0, temp;
-
-	h = *head;
-	while (h)
-	{
-		h = h->next;
-		length++;
-	}
-	if (length < 2)
-	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	h = *head;
-	if (h->n == 0)
-	{
-		fprintf(stderr, "L%d: division by zero\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE);
-	}
-	temp = h->next->n % h->n;
-	h->next->n = temp;
-	*head = h->next;
-	free(h);
-}
