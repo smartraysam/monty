450ef361802205166e0dc943187e10b7fcef1f43
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-06-22 23:46:05 +0100

    The mul opcode

---
+++ mul.c
@@ -0,0 +1,77 @@
+#include "monty.h"
+
+/**
+ * f_mul - multiply first 2 elements of the stack
+ * @head: pointer to pointer to first node
+ * @counter: line counter
+ *
+ * Return: none
+ */
+
+void f_mul(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int length = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	temp = h->next->n * h->n;
+	h->next->n = temp;
+	*head = h->next;
+	free(h);
+}
+
+/*
+ * f_mod - compute modulos of two elements in stack
+ * @head: pointer to pointer to first node
+ * @counter: line counter
+ *
+ * Return: none
+ */
+
+void f_mod(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int length = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	temp = h->next->n % h->n;
+	h->next->n = temp;
+	*head = h->next;
+	free(h);
+}
