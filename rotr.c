e7d71994eac820f0a50670239488bf9982d16d64
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-06-23 00:13:26 +0100

    The rotr opcode

--- rotr.c
+++ rotr.c
@@ -1,34 +1,6 @@
 #include "monty.h"
 
 /**
- * f_rotl- rotate stack element to the top
- * @head: first node of stack
- * @counter: line counter
- *
- * Return: none
- */
-
-void f_rotl(stack_t **head,  __attribute__((unused)) unsigned int counter)
-{
-	stack_t *tmp = *head, *aux;
-
-	if (*head == NULL || (*head)->next == NULL)
-	{
-		return;
-	}
-	aux = (*head)->next;
-	aux->prev = NULL;
-	while (tmp->next != NULL)
-	{
-		tmp = tmp->next;
-	}
-	tmp->next = *head;
-	(*head)->next = NULL;
-	(*head)->prev = tmp;
-	(*head) = aux;
-}
-
-/**
  * f_rotr - rotate stack element to the bottom
  * @head: first node of stack
  * @counter: line counter
