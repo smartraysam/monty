9c14db6f681c0763121b1a4257ca9438175e8063
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-06-22 23:00:56 +0100

    The add opcode

---
+++ add.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ * f_add - add first 2 elements of the stack
+ * @head: pointer to pointer to first node
+ * @counter: line counter
+ *
+ * Return: none
+ */
+
+void f_add(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int length = 0, temp;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	if (length < 2)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	temp = h->n + h->next->n;
+	h->next->n = temp;
+	*head = h->next;
+	free(h);
+}
+
+
