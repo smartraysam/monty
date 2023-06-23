3798fcf5443dbe07609957e466a88d7e67ec65a0
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-06-23 01:39:44 +0100

    betty compliance

--- func_exec.c
+++ func_exec.c
@@ -13,8 +13,8 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
 				{"push", f_push},
-			       	{"pall", f_pall},
-			       	{"pint", f_pint},
+				{"pall", f_pall},
+				{"pint", f_pint},
 				{"pop", f_pop},
 				{"swap", f_swap},
 				{"add", f_add},
